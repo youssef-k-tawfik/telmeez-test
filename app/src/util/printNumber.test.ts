747ce5527c1b58be10ee0d4b117dde95ad1d02bd
Youssef Tawfik <youssef.k.tawfik@gmail.com>
2024-12-25 23:09:43 +0200

    using env vars in production

--- app/src/util/printNumber.test.ts
+++ app/src/util/printNumber.test.ts
@@ -1,14 +1,20 @@
-import { printNumber } from './printNumber';
+import { printNumber } from "./printNumber";
+import dotenv from "dotenv";
 
-describe('printNumber', () => {
-  it('should return the input if it is a number', () => {
-    expect(printNumber(5)).toBe(5);
+describe("printNumber", () => {
+  beforeAll(() => {
+    dotenv.config();
+  });
+
+  it("should return the input if it is a number", () => {
+    expect(printNumber(Number(process.env.NUMBER_FIVE))).toBe(5);
+    expect(printNumber(process.env.NUMBER_SEVEN)).toBe(NaN);
     expect(printNumber(0)).toBe(0);
     expect(printNumber(-10)).toBe(-10);
   });
 
-  it('should return NaN if the input is not a number', () => {
-    expect(printNumber('5')).toBeNaN();
+  it("should return NaN if the input is not a number", () => {
+    expect(printNumber("5")).toBeNaN();
     expect(printNumber(null)).toBeNaN();
     expect(printNumber(undefined)).toBeNaN();
     expect(printNumber({})).toBeNaN();
